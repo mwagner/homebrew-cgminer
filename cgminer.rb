349cf6eb258927e31d9485f375e418dadef4705a
Marco Wagner <m.wagner@blue-design.ch>
2013-12-30 14:29:46 +0100

    first commit

---
+++ cgminer.rb
@@ -0,0 +1,41 @@
+require 'formula'
+
+class Cgminer < Formula
+  homepage 'https://github.com/ckolivas/cgminer'
+  head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
+  url 'https://github.com/ckolivas/cgminer/archive/v3.8.5.tar.gz'
+  sha1 'b3c1dce867363d137aa77f7da5652001bdbb4a3e'
+
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'coreutils' => :build
+  depends_on 'curl'
+
+  def install
+    inreplace "autogen.sh", "libtoolize", "glibtoolize"
+    inreplace "autogen.sh", "readlink", "greadlink"
+    system "autoreconf -fvi"
+    system "./autogen.sh", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/jansson/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libusb/lib/pkgconfig",
+                          "--enable-scrypt",
+                          "--enable-bflsc",
+                          "--enable-bitforce",
+                          "--enable-icarus",
+                          "--enable-modminer",
+                          "--enable-ztex",
+                          "--enable-avalon",
+                          "--disable-adl",
+                          "--enable-opencl",
+                          "--enable-bitfury",
+                          "--enable-hashfast",
+                          "--enable-klondike"
+    system "make", "install"
+  end
+
+  test do
+    system "cgminer"
+  end
+end
